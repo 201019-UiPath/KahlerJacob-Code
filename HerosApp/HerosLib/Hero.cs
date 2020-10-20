eab6e5b29480dfb82e8ea6daffa5ddcdcfb6f9cb
ZarKahler <jacobkahler8@gmail.com>
2020-10-20 14:14:59 -0500

    Updated Heros with getter and setter

---
+++ HerosApp/HerosLib/Hero.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HerosLib
+{
+    public class Hero
+    {
+        internal int id;
+        
+        internal string name;
+
+
+      
+        public Hero(){
+            id = 1;
+            name = "Don Quixote";
+
+        }
+       
+        public Hero(int newID, string newName)
+        {
+            this.id = newID;
+            this.name = newName;
+        }
+
+        public string GetName(){
+            return this.name;
+        }
+
+        public int GetId(){
+            return this.id;
+        }
+
+        public void SetName(string n){
+            name = n;
+        }
+
+        public void SetId(int i){
+            id = i;
+        }
+
+    }
+}
